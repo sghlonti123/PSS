143c501eaeedfb3c5a1a8a79441174096add3ba7
sghlonti <shalva.ghlonti.1@btu.edu.ge>
2023-09-03 20:01:32 +0400

    added some functions

--- first.py
+++ first.py
@@ -1,13 +1,38 @@
 
-a = 1
+# comment goes here
 
-b = "Salome"
+a = 1 # int aka integer
 
-c = a
+b = "Salome" # str string
 
-print("Hello World")
+c = a # equals to variable
 
-def custom_function():
+print("Hello World") # function that already exists
+
+def custom_function(): # custom function
     print("Hello World From Custom Function")
 
-custom_function()
+custom_function() # calling a function
+
+def mathematics(): # math operations
+    print(1+1)
+    print(2-1)
+    print(3*4)
+    print(4/2)
+
+mathematics()
+
+def talk_to_me():
+    name = input("Tell me your name: ") # new function - input
+    print("Nice to meet you", name) # several arguments in function
+    age = input("Tell me your age: ")
+    print("Oh you're ", age, ", you will 100 in ", 100-age)
+
+talk_to_me()
+
+number = int("1")
+age = int(input("Tell me your age: "))
+
+
+# assignment: create function which tells me how many years older user is compared to you
+
