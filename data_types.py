34a73f62c2313648fb3a790f775672ea7ae1a7be
sghlonti <shalva.ghlonti.1@btu.edu.ge>
2023-10-02 03:01:24 +0400

    Added Few examples

--- data_types.py
+++ data_types.py
@@ -18,3 +18,22 @@ some_other_bool = False
 
 # Nonetype
 none_type = None
+
+
+# Numeric
+integer_2 = int(100)
+floats_2 = float(3.7)
+
+# String
+string_2 = str("Some String")
+
+# Collections
+example_list_2 = list([1,2,3])
+example_tuple_2 = tuple((3,4,5))
+example_range_2 = range(10)
+example_dict_2 = dict({1:"Shako", 2:"Salome", 3:"Giorgi"})
+example_set_2 = set({"set_item_one", "set_item_two"})
+
+# Booleans
+some_bool_2 = bool(True)
+some_other_bool_2 = bool(False)
